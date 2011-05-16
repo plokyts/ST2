a7fa61da376de3e433ec8d006bb77607166ae21d
Piotr Zbyszyński <p.zbyszynski@binarysolutions.com.pl>
2011-05-17 01:58:33 +0200

    Po mergu

--- ST2/ST2/ST2AppDelegate.h
+++ ST2/ST2/ST2AppDelegate.h
@@ -2,9 +2,9 @@
 //  ST2AppDelegate.h
 //  ST2
 //
-//  Created by Piotr Zbyszyński on 11-05-17.
+//  Created by Piotr Zbyszy≈Ñski on 11-05-17.
 //  Copyright 2011 Binary Solutions. All rights reserved.
-//
+//ł
 
 #import <UIKit/UIKit.h>
 
